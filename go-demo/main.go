915771a86b034e509102b4eda302b8ed7ef3ed3e
Zhanaidar <zhanikss@gmail.com>
2025-09-05 14:27:48 +0500

    main.go_5

--- go-demo/main.go
+++ go-demo/main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
+	const IMTpower = 2
 	userHeight := 1.8
-	userWeigt := 100
-	IMT := float64 (userWeigt) / math.Pow (userHeight, 2)
-	fmt.Print(IMT)
+	userWeigt := 100.0
+	fmt.Println("___ Калькулятор индекса массы ___")
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес в кг: ")
+	fmt.Scan(&userWeigt)
+	IMT := (userWeigt) / math.Pow (userHeight / 100, IMTpower)
+	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
 }
