dd8b22373934659dd9d27901a028b4ea431dcb11
Guest User <zhanikss@egmail.com>
2025-09-06 19:42:32 +0500

    main.go_8

--- go-demo/main.go
+++ go-demo/main.go
@@ -6,18 +6,29 @@ import (
 )
 
 func main() {
-	const IMTpower = 2
 	var userHeight float64
 	var userWeight float64
+	userWeight, userHeight = getUserInput(userWeight, userHeight)
 	fmt.Println("___ Калькулятор индекса массы ___")
+	IMT := calculateIMT(userWeight, userHeight)
+	outputResult(IMT)
+}
+
+func outputResult(imt float64) {
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
+	fmt.Print(result)
+}
+
+func calculateIMT(userWeight float64, userHeight float64) float64{
+	const IMTpower = 2
+	IMT := userWeight / math.Pow (userHeight / 100, IMTpower)
+	return IMT
+}
+
+func getUserInput(userWeight float64, userHeight float64) (float64, float64) {
 	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес в кг: ")
 	fmt.Scan(&userWeight)
-	IMT := (userWeight) / math.Pow (userHeight / 100, IMTpower)
-	
-}
-func outputResult(imt float64) {
-result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
-	fmt.Print(result)
+	return userWeight, userHeight
 }
