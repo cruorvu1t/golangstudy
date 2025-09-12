af977dd7e3c234a1f0af521f2b9cdb994c775643
Zhanaidar <zhanikss@gmail.com>
2025-09-12 14:03:29 +0500

    replace file main.go

--- go-demo_01/main.go
+++ go-demo_01/main.go
@@ -1,8 +1,212 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Hello, World!")
+
+	subscribed := false
+	
+	fmt.Println("До условия", subscribed)
+	
+		if !subscribed {
+		fmt.Println("Я вижу ты не подписан. Подпишись пожалуйста!")
+	}
+	
+	
+//	if subscribed {
+//
+//	} else {
+//		fmt.Println("Я вижу ты не подписан. Подпишись пожалуйста!")
+//		}
+	
+	fmt.Println("после условия", subscribed)
+
+/*	sunny := true
+	weekend := false
+
+	if sunny && weekend{
+		fmt.Println("Я иду гулять")
+		} else {
+			fmt.Println("Я сопротивляюсь")
+		}
+	
+	computerClub := true
+	icecream := false
+	
+	if computerClub || icecream {
+		fmt.Println("Я иду гулять")	
+		} else {
+			fmt.Println("Я сопротивляюсь")
+		} */
+		
+/*	score := 5
+	if score > 6 && score < 15 {
+		fmt.Println("Ты попал в яблочко!")
+		} else {
+			fmt.Println("Ты не попал в яблочко")
+			}
+		
+
+
+
+
+
+//	score := 21
+	if score <6 || score > 16{
+	fmt.Println("Ты попал в зону")
+	} else {
+		fmt.Println("Ты не попал в зону")
+		}
+	*/
+
+
+
+
+
+
+
+
+/*	number := 15
+
+	ravno5 := number == 5
+	bolshe12 := number > 12
+
+	fmt.Println(ravno5)
+	fmt.Println(bolshe12)*/
+
+	/*score := 50
+
+	if score == 12 {
+		fmt.Println("Дюжина!")
+	} else if score == 21{
+		fmt.Println("Очко")
+		} else if score == 50 {
+			fmt.Println("Полтинник!")
+			} else {
+			fmt.Println("Ты не попал ни в какую категорию")
+	}*/
+
+	/*score := 16
+	if score > 15 {
+		fmt.Println("Ты мега-красавчик!")
+	} else if score > 10 {
+		fmt.Println("Ты красавчик!")
+	} else {
+		fmt.Println("Тебе нужно еще многому научиться")
+
+	}
+
+	fmt.Println("Счёт: ", score)
+	fmt.Println("Спасибо за использование нашей игры! Возвращайтесь еще!") */
+
+	/*
+		score := 10
+
+		if score > 10 {
+				if score > 15 {
+					fmt.Println("Ты мега-красавчик!")
+				} else {
+					fmt.Println("Ты красавчик!")
+				}
+			} else {
+				fmt.Println("Тебе нужно еще многому научиться")
+			}
+
+			fmt.Println("Счёт: ", score)
+			fmt.Println("Спасибо за использование нашей игры! Возвращайтесь еще!")
+	*/
+	/*	number := 7
+
+		number ++
+
+		number --
+
+		fmt.Println("number:", number)
+
+		text := "hello"
+		text += " world"
+
+		fmt.Println("text: ", text) */
+
+	/*	var number int
+		var text string
+		var drob float64
+		var boolean bool
+
+		fmt.Println("number:", number)
+		fmt.Println("text:", text)
+		fmt.Println("drob:", drob)
+		fmt.Println("boolean:", boolean) */
+
+	/*	var number int
+		var text string
+
+		fmt.Println("number", number)
+		fmt.Println("text", text)
+
+		number = 15
+		text = "Hello world"
+
+		fmt.Println("number", number)
+		fmt.Println("text", text) */
+
+	/*	score := 12
+		s := "hello"
+
+		var number int = 10
+		var text string = "hi" */
+
+	/*	text := "hello"
+		text = text + " world"
+		fmt.Println(text) */
+
+	/*	score := 11
+			celoe := score / 3
+			ostatok := score % 3
+
+		//		score = score + 10
+		//		score = score + 20
+		//		score = score - 15
+		//		score = score / 3
+		//		score = score * 10
+
+			fmt.Println("Счёт: ", score, celoe, ostatok) */
+
+	/*
+		text :="hello"
+		score := 0
+		drob := 0.5
+
+		fmt.Println(text)
+		fmt.Println(score)
+		fmt.Println(drob)
+
+		subscribed := false
+
+		fmt.Println("Подписан на Мистера Биста?", subscribed)
+	*/
+
+	/*
+	   text1 := "Get Ready"
+	   text2 := "Game Over"
+	   score := 0
+
+	   fmt.Println(text1, score)
+	   fmt.Println("Ваш счёт", score)
+	   score = score + 1
+
+	   fmt.Println("Вы пролетели через трубу", score)
+	   fmt.Println("Ваш счёт", score)
+	   score = score + 1
+	   fmt.Println("Вы пролетели через трубу", score)
+
+	   fmt.Println("Ваш счёт", score)
+	   score = score + 1
+	   fmt.Println("Вы пролетели через трубу", score)
+
+	   fmt.Println("Врезались в трубу")
+	   fmt.Println(text2)
+	*/
 }
